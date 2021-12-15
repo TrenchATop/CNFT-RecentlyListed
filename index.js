84a1f25a1c691844eff28c731c1214c9fe2ab034
TrenchATop <62423508+TrenchATop@users.noreply.github.com>
2021-12-15 16:45:36 +1300

    Faster function calls
    Reduced code size

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const { MessageEmbed } = require('discord.js');
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 const payLoad = {"search":"","types":["listing","offer"],"project":"The Pixel Head Squad","sort":{"_id":-1},"priceMin":null,"priceMax":null,"page":1,"verified":true,"nsfw":false,"sold":false,"smartContract":false}
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-function getColour(Data, price){
-    if (Data.smartContractTxid) {
+
+function getColour(data, price){
+    if (data.smartContractTxid) {
         const color =  '#fbff00';
         return color;
     }else if (price > 2000) {
@@ -22,24 +22,24 @@ function getColour(Data, price){
 }
 async function getTag() {
     let listedItems = await axios.post(`https://api.cnft.io/market/listings`, payLoad);
-    let tag = listedItems.headers['etag'];
-    let newData = listedItems.data.results[0];
-    let listings = listedItems.data.count;
-    return {tag, newData, listings}
+    let tag = listedItems.headers.etag;
+    let data = listedItems.data.results[0];
+    let listings = listedItems.data.count
+    return [tag, listings, data]
 }
 
-function discordListing(Data) {
+function discordListing(data) {
     const channel = client.channels.cache.get('919165272705876008');
-    let name = Data.asset.metadata.name
+    let name = data.asset.metadata.name
     let nameGif = name.substr(-3)
-    let price = Data.price/1000000
-    let listing = Data._id
+    let price = data.price/1000000
+    let listing = data._id
     let link = `https://cnft.io/token/${listing}`
-    let smartContract = (Data.smartContractTxid) ? "Yes":"No";
+    let smartContract = (data.smartContractTxid) ? "Yes":"No";
     let image = `https://pixelhead.io/images/tokens/${nameGif}1.gif`
 
     const exampleEmbed = new MessageEmbed()
-        .setColor(getColour(Data, price))
+        .setColor(getColour(data, price))
         .setTitle(name)
         .setURL(link)
         .setAuthor('Recently listed Pixel Head Squad')
@@ -54,34 +54,38 @@ function discordListing(Data) {
 
     channel.send({ embeds: [exampleEmbed] });
 }
+
 async function checkTag() {
-    while (true) {
-        let currentTag = (await getTag()).tag;
-        let currentListings = (await getTag()).listings;
-        await delay(2000);
-        let newListings = (await getTag()).listings;
+    while(true) {
+      
+      const [currentTag, currentListings] = await getTag()
+  
+      await delay(4000);
+      
+      const [newTag, newListings, data] = await getTag()
+  
+      if (currentTag != newTag) {
 
-        if(currentTag == (await getTag()).tag) {
-            continue;
-            
+        if (currentListings < newListings) {
+  
+          console.log("New Listing")
+          discordListing(data)
+  
+        }else if (currentListings == newListings) {
+  
+          console.log("New Offer BAC")
+
         }else {
-            console.log("Tags are different")
+  
+          console.log("Delisted/Sold BAC")
 
-            if(currentListings < newListings) {
-                console.log(currentListings - newListings)
-                console.log("New listing")
-
-                let Data = (await getTag()).newData;
-                discordListing(Data)
-                console.log('finished')
-
-            } else {
-                console.log(currentListings - newListings)
-                console.log("Something has sold/delisted")
-            }  
         }
+  
+      }else {
+        
+      }
     }
-}
+  }
 client.once('ready', () => {
     checkTag();
 });
